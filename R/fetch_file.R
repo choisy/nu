f24e5408ba0742a88bed2b1ab0eef22646ab38db
Marc Choisy <marc.choisy@ird.fr>
2021-02-20 12:39:20 +0700

    Adding fetch_file()

---
+++ R/fetch_file.R
@@ -0,0 +1,5 @@
+fetch_file <- function(file, ip, dir, user, key) {
+  system(paste0("sftp -i ", key, " ", user, "@", ip, ":", dir, " <<< $'get ", file, "'"))
+}
+
+#fetch_file("sim1.rds", nu::c1, "Marc", "ubuntu", "~/.ssh/GROUP-Modelling.pem")
